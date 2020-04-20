98c15de93252bb202c9314018bfa5cd824bea86d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 20:34:12 +0700

    ADD COMMIT

--- node/server.js
+++ node/server.js
@@ -4,6 +4,30 @@
 
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
+
+//แปลง JSON File ใข้ BosyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello');
+});
+
+
+app.get('/home', (req, res) =>{
+    res.send('Hello Home');
+});
+
+app.post('/api', (req, res) =>{
+
+    const feedback = req.body.feedback;
+    //req.body.feedBAck คือการใช้ BodyParser ในการอ่าน request ที่เข้ามาและทำการแปลง
+
+    res.end("Feedback : " + feedback);
+    
+});
 
 app.listen(3000, ()=> {
 
