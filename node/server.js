6c54605e9878d8eef8d98e6a7566c50ba02f1213
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 20:24:51 +0700

    add server backend

--- node/server.js
+++ node/server.js
@@ -1,3 +1,11 @@
 //Backend
+//ใช้ nodemon run backend
 
-console.log('Wellcome to Jan');
+
+const express = require('express')
+const app = express();
+
+app.listen(3000, ()=> {
+
+    console.log("Server is Running at PORT 3000");
+} )
