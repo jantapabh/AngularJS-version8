123327671e89f8ab69eb0fc5e335ffbddc11c996
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 20:43:59 +0700

    Add module

--- node/server.js
+++ node/server.js
@@ -11,25 +11,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Hello');
 });
 
 
-app.get('/home', (req, res) =>{
+app.get('/home', (req, res) => {
     res.send('Hello Home');
 });
 
-app.post('/api', (req, res) =>{
+app.post('/api', (req, res) => {
 
     const feedback = req.body.feedback;
     //req.body.feedBAck คือการใช้ BodyParser ในการอ่าน request ที่เข้ามาและทำการแปลง
 
     res.end("Feedback : " + feedback);
-    
+
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
 
     console.log("Server is Running at PORT 3000");
-} )
+})
