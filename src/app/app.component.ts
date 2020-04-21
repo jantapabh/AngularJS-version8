03ec41d708b3e093a8875ae3e5fcabef5b27784a
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-21 14:00:41 +0700

    Add commit last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 
 
 
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
 
 @Component({
-    selector: 'app-top-bar',
+    selector: 'top-bar-component',
     templateUrl: './top-bar.component.html',
     styleUrls: ['./top-bar.component.css']
 })
