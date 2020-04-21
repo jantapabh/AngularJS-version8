ba41fd466655407010684ee949fba88fe8152e8e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-22 00:20:46 +0700

    ADD COMMIT API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/random_masks_usage_instructions.php',
+  apiUrl: 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php',
   apiHost: 'coronavirus-monitor.p.rapidapi.com',
   apiKey: 'f631e4e92fmsh7a91d3285e63f94p1ace56jsn2b13b13b6f17'
 };
