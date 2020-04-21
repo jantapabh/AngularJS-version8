003895cb888700cb11150c92f0c42a5e67d05b3d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-21 13:23:31 +0700

    ADD COMPONENTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,35 +14,12 @@ export class AppComponent implements OnInit{
 
 
   title = 'JANTAPA';
-  mDataArray:any[] = []
 
-  constructor(private http:HttpClient) {
-
-
-  }
-
-//ส่วน onSubmit ในฟอร์มที่เราใส่ไว้
-  onSubmit(data){
-
-    alert(JSON.stringify(data));
-    // let postData = {username: data.email, feedback: data.feedback};
-    this.http.post<any>('http://localhost:3000/api', data).subscribe(result => {
-      alert(JSON.stringify(result));
-    })
-  }
-
-  getFeedback(){
-    this.http.get<any>('http://localhost:3000/api').subscribe(result => {
-      //alert(JSON.stringify(result));
-      this.mDataArray = result.data;
-    });
-  }
-
-  //เป็นการยิงข้อมูลไปยังฝั่งแสดงผลบน server โดยกำหนดให้ยิงไปที่ สนแฟส้นหะ ที่กำหนดได้
+//ส่วน onSubmit ในฟอร์มที่เราใส่ไว
+//เป็นการยิงข้อมูลไปยังฝั่งแสดงผลบน server โดยกำหนดให้ยิงไปที่ สนแฟส้นหะ ที่กำหนดได้
 
   ngOnInit(): void {
 
-    this.getFeedback();
 
   }
 
